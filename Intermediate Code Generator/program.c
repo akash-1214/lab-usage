738dab2ddaa9f1c8439be0cc9ef174a38dd84c3f
AKASH-KUMAR-R <user.name>
2025-10-17 00:33:17 +0530

    fix: issue resolbed

--- Intermediate Code Generator/program.c
+++ Intermediate Code Generator/program.c
@@ -1,45 +1,42 @@
 #include <stdio.h>
-#include <string.h>
-char op[2], arg1[5], arg2[5], result[5];
+#include <ctype.h>
+
 int main()
 {
-	FILE *fp1, *fp2;
-	fp1 = fopen("input.txt", "r");
-	fp2 = fopen("output.txt", "w");
-	while (!feof(fp1))
-	{
+	char expr[50];
+	char stack[50][10];
+	int top = -1, t = 1;
+	char op, a, b;
 
-		fscanf(fp1, "%s%s%s%s", op, arg1, arg2, result);
-		if (strcmp(op, "+") == 0)
+	printf("Enter an arithmetic expression (like b*c+a): ");
+	scanf("%s", expr);
+
+	printf("\nThree Address Code:\n");
+
+	for (int i = 0; expr[i] != '\0'; i++)
+	{
+		// If it's an operator
+		if (expr[i] == '+' || expr[i] == '-' || expr[i] == '*' || expr[i] == '/')
 		{
-			fprintf(fp2, "\nMOV R0,%s", arg1);
-			fprintf(fp2, "\nADD R0,%s", arg2);
-			fprintf(fp2, "\nMOV %s,R0", result);
-		}
-		if (strcmp(op, "*") == 0)
-		{
-			fprintf(fp2, "\nMOV R0,%s", arg1);
-			fprintf(fp2, "\nMUL R0,%s", arg2);
-			fprintf(fp2, "\nMOV %s,R0", result);
-		}
-		if (strcmp(op, "-") == 0)
-		{
-			fprintf(fp2, "\nMOV R0,%s", arg1);
-			fprintf(fp2, "\nSUB R0,%s", arg2);
-			fprintf(fp2, "\nMOV %s,R0", result);
-		}
-		if (strcmp(op, "/") == 0)
-		{
-			fprintf(fp2, "\nMOV R0,%s", arg1);
-			fprintf(fp2, "\nDIV R0,%s", arg2);
-			fprintf(fp2, "\nMOV %s,R0", result);
-		}
-		if (strcmp(op, "=") == 0)
-		{
-			fprintf(fp2, "\nMOV R0,%s", arg1);
-			fprintf(fp2, "\nMOV %s,R0", result);
+			op = expr[i];
+			b = expr[i + 1]; // right operand
+			a = expr[i - 1]; // left operand
+
+			if (a == 't')
+			{
+				printf("t%d = t%d %c %c\n", t, t - 1, op, b);
+			}
+			else
+			{
+				printf("t%d = %c %c %c\n", t, a, op, b);
+			}
+
+			expr[i + 1] = 't'; // replace left operand with result
+			expr[i] = '0' + t; // mark temp variable number
+			t++;
 		}
 	}
-	fclose(fp1);
-	fclose(fp2);
+
+	printf("\nFinal result stored in: t%d\n", t - 1);
+	return 0;
 }
